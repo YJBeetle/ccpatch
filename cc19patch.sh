510167cab1252541b1b0feab0da4b7f306c8d0d1
YJBeetle <yjbeetle@gmail.com>
2019-07-07 16:20:55 +0800

    mac shell update

--- cc19patch.sh
+++ cc19patch.sh
@@ -1,130 +1,245 @@
 #!/bin/bash
-echo 'Patch PS CC 19'
-PSCC19='/Applications/Adobe Photoshop CC 2019/Adobe Photoshop CC 2019.app/Contents/MacOS/Adobe Photoshop CC 2019'
-if [ ! -f "$PSCC19.bak" ]; then
-    sudo mv "$PSCC19" "$PSCC19.bak"
-    sudo cp "$PSCC19.bak" "$PSCC19"
-    echo "Backup file: $PSCC19.bak"
-fi
-sudo perl -pi -e 's|\x0F\xB6\x32\x48\x83\xFE\x20|\x90\x90\x90\x48\x83\xFE\x20|g' "$PSCC19"
-sudo perl -pi -e 's|\x48\x39\xD0\x75\xE2|\x48\x39\xD0\x90\x90|g' "$PSCC19"
-echo 'Patch complete'
 
-echo 'Patch AI CC 19'
-AICC19='/Applications/Adobe Illustrator CC 2019/Adobe Illustrator.app/Contents/MacOS/Adobe Illustrator'
-if [ ! -f "$AICC19.bak" ]; then
-    sudo mv "$AICC19" "$AICC19.bak"
-    sudo cp "$AICC19.bak" "$AICC19"
-    echo "Backup file: $AICC19.bak"
-fi
-sudo perl -pi -e 's|\x0F\xB6\x10\x48\x83\xFA\x20|\x90\x90\x90\x48\x83\xFA\x20|g' "$AICC19"
-sudo perl -pi -e 's|\x48\x39\xC3\x75\xE1|\x48\x39\xC3\x90\x90|g' "$AICC19"
-echo 'Patch complete'
+function back_rel()
+{
+    __file=$1
+    cp "$__file" "$__file.bak"
+    echo "Backup finished."
+}
 
-echo 'Patch PR CC 19'
-PRCC19='/Applications/Adobe Premiere Pro CC 2019/Adobe Premiere Pro CC 2019.app/Contents/Frameworks/Registration.framework/Versions/A/Registration'
-if [ ! -f "$PRCC19.bak" ]; then
-    sudo mv "$PRCC19" "$PRCC19.bak"
-    sudo cp "$PRCC19.bak" "$PRCC19"
-    echo "Backup file: $PRCC19.bak"
-fi
-sudo perl -pi -e 's|\x0F\xB6\x32\x48\x83\xFE\x20\x77\x1B\x48\x0F\xA3\xF1\x73\x15\x48\xFF\xC2\x49\x89\x97\x88\x00\x00\x00\x48\x39\xD0\x75\xE2\x49\x89\x56\x08\xEB\x1C\x49\x89\x56\x08\x48\x39\xC2\x74\x13\x48\x8D\x4A\x01\x49\x89\x8F\x88\x00\x00\x00\x40\x8A\x32\x48\x89\xCA\xEB\x05\x31\xF6\x48\x89\xC1\x40\x0F\xBE\xF6\x83\xFE\x7D\x0F\x87\x55\x03|\x90\x90\x90\x48\x83\xFE\x20\x77\x1B\x48\x0F\xA3\xF1\x73\x15\x48\xFF\xC2\x49\x89\x97\x88\x00\x00\x00\x48\x39\xD0\x90\x90\x49\x89\x56\x08\xEB\x1C\x49\x89\x56\x08\x48\x39\xC2\x74\x13\x48\x8D\x4A\x01\x49\x89\x8F\x88\x00\x00\x00\x40\x8A\x32\x48\x89\xCA\xEB\x05\x31\xF6\x48\x89\xC1\x40\x0F\xBE\xF6\x83\xFE\x7D\x0F\x87\x55\x03|g' "$PRCC19"
-echo 'Patch complete'
+function back()
+{
+    __file=$1
+    
+    if [ ! -f "$__file.bak" ]; then
+        back_rel "$__file"
+        return 0
+    elif [ ! -f "$__file.patched.crc32" ]; then
+        back_rel "$__file"
+        return 0
+    elif [ $(cat "$__file.patched.crc32") != $(crc32 "$__file") ]; then
+        back_rel "$__file"
+        return 0
+    else
+        echo "Already patched."
+        return 1
+    fi
+}
 
-echo 'Patch AE CC 19'
-AECC19='/Applications/Adobe After Effects CC 2019/Adobe After Effects CC 2019.app/Contents/Frameworks/AfterFXLib.framework/Versions/A/AfterFXLib'
-if [ ! -f "$AECC19.bak" ]; then
-    sudo mv "$AECC19" "$AECC19.bak"
-    sudo cp "$AECC19.bak" "$AECC19"
-    echo "Backup file: $AECC19.bak"
-fi
-sudo perl -pi -e 's|\x0F\xB6\x32\x48\x83\xFE\x20|\x90\x90\x90\x48\x83\xFE\x20|g' "$AECC19"
-sudo perl -pi -e 's|\x48\x39\xD0\x75\xE2|\x48\x39\xD0\x90\x90|g' "$AECC19"
-echo 'Patch complete'
+function patch()
+{
+    __file=$1
+    __find=$2
+    __to=$3
 
-echo 'Patch AU CC 19'
-AUCC19='/Applications/Adobe Audition CC 2019/Adobe Audition CC 2019.app/Contents/Frameworks/AuUI.framework/Versions/A/AuUI'
-if [ ! -f "$AUCC19.bak" ]; then
-    sudo mv "$AUCC19" "$AUCC19.bak"
-    sudo cp "$AUCC19.bak" "$AUCC19"
-    echo "Backup file: $AUCC19.bak"
-fi
-sudo perl -pi -e 's|\x0F\xB6\x32\x48\x83\xFE\x20\x77\x1B\x48\x0F\xA3\xF1\x73\x15\x48\xFF\xC2\x49\x89\x97\x88\x00\x00\x00\x48\x39\xD0\x75\xE2\x49\x89\x56\x08\xEB\x1C\x49\x89\x56\x08\x48\x39\xC2\x74\x13\x48\x8D\x4A\x01\x49\x89\x8F\x88\x00\x00\x00\x40\x8A\x32\x48\x89\xCA\xEB\x05\x31\xF6\x48\x89\xC1\x40\x0F\xBE\xF6\x83\xFE\x7D\x0F\x87\x55\x03|\x90\x90\x90\x48\x83\xFE\x20\x77\x1B\x48\x0F\xA3\xF1\x73\x15\x48\xFF\xC2\x49\x89\x97\x88\x00\x00\x00\x48\x39\xD0\x90\x90\x49\x89\x56\x08\xEB\x1C\x49\x89\x56\x08\x48\x39\xC2\x74\x13\x48\x8D\x4A\x01\x49\x89\x8F\x88\x00\x00\x00\x40\x8A\x32\x48\x89\xCA\xEB\x05\x31\xF6\x48\x89\xC1\x40\x0F\xBE\xF6\x83\xFE\x7D\x0F\x87\x55\x03|g' "$AUCC19"
-echo 'Patch complete'
+    perl -pi -e "s|$__find|$__to|g" "$__file"
+    crc32 "$__file" > "$__file.patched.crc32"
+    echo "Patch finished."
+}
 
-echo 'Patch ME CC 19'
-MECC19='/Applications/Adobe Media Encoder CC 2019/Adobe Media Encoder CC 2019.app/Contents/MacOS/Adobe Media Encoder CC 2019'
-if [ ! -f "$MECC19.bak" ]; then
-    sudo mv "$MECC19" "$MECC19.bak"
-    sudo cp "$MECC19.bak" "$MECC19"
-    echo "Backup file: $MECC19.bak"
-fi
-sudo perl -pi -e 's|\x0F\xB6\x32\x48\x83\xFE\x20|\x90\x90\x90\x48\x83\xFE\x20|g' "$MECC19"
-sudo perl -pi -e 's|\x48\x39\xD0\x75\xE2|\x48\x39\xD0\x90\x90|g' "$MECC19"
-echo 'Patch complete'
+function run()
+{
+    __tab=$1
+    __file=$2
+    __find=$3
+    __to=$4
 
-echo 'Patch LR CC 19'
-LRCC19='/Applications/Adobe Lightroom Classic CC/Adobe Lightroom Classic CC.app/Contents/MacOS/Adobe Lightroom Classic'
-if [ ! -f "$LRCC19.bak" ]; then
-    sudo mv "$LRCC19" "$LRCC19.bak"
-    sudo cp "$LRCC19.bak" "$LRCC19"
-    echo "Backup file: $LRCC19.bak"
-fi
-sudo perl -pi -e 's|\x0F\xB6\x30\x48\x83\xFE\x20\x77\x1B|\x90\x90\x90\x48\x83\xFE\x20\x77\x1B|g' "$LRCC19"
-sudo perl -pi -e 's|\x73\x15\x48\xFF\xC0\x48\x89\x83\x88\x00\x00\x00\x48\x39\xC1\x75\xE2|\x73\x15\x48\xFF\xC0\x48\x89\x83\x88\x00\x00\x00\x48\x39\xC1\x90\x90|g' "$LRCC19"
-echo 'Patch complete'
+    if [ -f "$__file" ]; then
+        echo "$__tab"
+        back "$__file" && patch "$__file" "$__find" "$__to"
+    fi
+}
 
-echo 'Patch BR CC 19'
-BRCC19='/Applications/Adobe Bridge CC 2019/Adobe Bridge CC 2019.app/Contents/MacOS/Adobe Bridge CC 2019'
-if [ ! -f "$BRCC19.bak" ]; then
-    sudo mv "$BRCC19" "$BRCC19.bak"
-    sudo cp "$BRCC19.bak" "$BRCC19"
-    echo "Backup file: $BRCC19.bak"
-fi
-sudo perl -pi -e 's|\x0F\xB6\x31\x48\x83\xFE\x20|\x90\x90\x90\x48\x83\xFE\x20|g' "$BRCC19"
-sudo perl -pi -e 's|\x48\x39\xC8\x75\xE2|\x48\x39\xC8\x90\x90|g' "$BRCC19"
-echo 'Patch complete'
+function Ps()
+{
+    run \
+        'Patch Ps ...' \
+        '/Applications/Adobe Photoshop CC 2019/Adobe Photoshop CC 2019.app/Contents/MacOS/Adobe Photoshop CC 2019' \
+        "\x66\x41\x8B\x5D\x08\x84\xDB\x74\x09\x80\xFB\x07" \
+        "\x66\x41\x8B\x5D\x08\xB3\x01\x74\x09\x80\xFB\x07"
+}
 
-echo 'Patch DW CC 19'
-DWCC19='/Applications/Adobe Dreamweaver CC 2019/Adobe Dreamweaver CC 2019.app/Contents/MacOS/Dreamweaver'
-if [ ! -f "$DWCC19.bak" ]; then
-    sudo mv "$DWCC19" "$DWCC19.bak"
-    sudo cp "$DWCC19.bak" "$DWCC19"
-    echo "Backup file: $DWCC19.bak"
-fi
-sudo perl -pi -e 's|\x0F\xB6\x32\x48\x83\xFE\x20|\x90\x90\x90\x48\x83\xFE\x20|g' "$DWCC19"
-sudo perl -pi -e 's|\x48\x39\xD0\x75\xE2|\x48\x39\xD0\x90\x90|g' "$DWCC19"
-echo 'Patch complete'
+function Lr()
+{
+    run \
+        'Patch Lr ...' \
+        '/Applications/Adobe Lightroom Classic CC/Adobe Lightroom Classic CC.app/Contents/MacOS/Adobe Lightroom Classic' \
+        "\x49\x89\xD4\x49\x89\xF7\x49\x89\xFE\x66\x41\x8B\x5E\x08\x84\xDB\x0F\x84\xF7\x00\x00\x00\x80\xFB\x07" \
+        "\x49\x89\xD4\x49\x89\xF7\x49\x89\xFE\x66\x41\x8B\x5E\x08\xB3\x01\x0F\x84\xF7\x00\x00\x00\x80\xFB\x07"
+}
 
-echo 'Patch CH CC 19'
-CHCC19='/Applications/Adobe Character Animator CC 2019/Adobe Character Animator CC 2019.app/Contents/MacOS/Character Animator'
-if [ ! -f "$CHCC19.bak" ]; then
-    sudo mv "$CHCC19" "$CHCC19.bak"
-    sudo cp "$CHCC19.bak" "$CHCC19"
-    echo "Backup file: $CHCC19.bak"
-fi
-sudo perl -pi -e 's|\x0F\xB6\x32\x48\x83\xFE\x20|\x90\x90\x90\x48\x83\xFE\x20|g' "$CHCC19"
-sudo perl -pi -e 's|\x48\x39\xD0\x75\xE2|\x48\x39\xD0\x90\x90|g' "$CHCC19"
-echo 'Patch complete'
+function Ai()
+{
+    run \
+        'Patch Ai ...' \
+        '/Applications/Adobe Illustrator CC 2019/Adobe Illustrator.app/Contents/MacOS/Adobe Illustrator' \
+        "\x66\x41\x8B\x5E\x08\x84\xDB\x74\x09\x80\xFB\x07" \
+        "\x66\x41\x8B\x5E\x08\xB3\x01\x74\x09\x80\xFB\x07"
+}
 
-echo 'Patch AN CC 19'
-ANCC19='/Applications/Adobe Animate CC 2019/Adobe Animate CC 2019.app/Contents/MacOS/Adobe Animate CC 2019'
-if [ ! -f "$ANCC19.bak" ]; then
-    sudo mv "$ANCC19" "$ANCC19.bak"
-    sudo cp "$ANCC19.bak" "$ANCC19"
-    echo "Backup file: $ANCC19.bak"
-fi
-sudo perl -pi -e 's|\x0F\xB6\x32\x48\x83\xFE\x20|\x90\x90\x90\x48\x83\xFE\x20|g' "$ANCC19"
-sudo perl -pi -e 's|\x48\x39\xD0\x75\xE2|\x48\x39\xD0\x90\x90|g' "$ANCC19"
-echo 'Patch complete'
+function Id()
+{
+    run \
+        'Patch Id ...' \
+        '/Applications/Adobe InDesign CC 2019/Adobe InDesign CC 2019.app/Contents/MacOS/PublicLib.dylib' \
+        "\x41\x0F\xB6\x47\x08\x84\xC0\x74\x08\x3C\x07" \
+        "\x41\x0F\xB6\x47\x08\xB0\x01\x74\x08\x3C\x07"
+}
 
-echo 'Patch DN CC 19'
-DNCC19='/Applications/Adobe Dimension CC/Adobe Dimension CC.app/Contents/Frameworks/amtlib.framework/Versions/A/amtlib'
-if [ ! -f "$DNCC19.bak" ]; then
-    sudo mv "$DNCC19" "$DNCC19.bak"
-    sudo cp "$DNCC19.bak" "$DNCC19"
-    echo "Backup file: $DNCC19.bak"
+function Ic()
+{
+    run \
+        'Patch Ic ...' \
+        '/Applications/Adobe InCopy CC 2019/Adobe InCopy CC 2019.app/Contents/MacOS/PublicLib.dylib' \
+        "\x41\x0F\xB6\x47\x08\x84\xC0\x74\x08\x3C\x07" \
+        "\x41\x0F\xB6\x47\x08\xB0\x01\x74\x08\x3C\x07"
+}
+
+function Au()
+{
+    run \
+        'Patch Au ...' \
+        '/Applications/Adobe Audition CC 2019/Adobe Audition CC 2019.app/Contents/Frameworks/AuUI.framework/Versions/A/AuUI' \
+        "\x49\x89\xD7\x49\x89\xF6\x49\x89\xFD\x66\x41\x8B\x5D\x08\x84\xDB\x74\x09\x80\xFB\x07" \
+        "\x49\x89\xD7\x49\x89\xF6\x49\x89\xFD\x66\x41\x8B\x5D\x08\xB3\x01\x74\x09\x80\xFB\x07"
+}
+
+function Pr()
+{
+    run \
+        'Patch Pr ...' \
+        '/Applications/Adobe Premiere Pro CC 2019/Adobe Premiere Pro CC 2019.app/Contents/Frameworks/Registration.framework/Versions/A/Registration' \
+        "\x49\x89\xD7\x49\x89\xF6\x49\x89\xFD\x66\x41\x8B\x5D\x08\x84\xDB\x74\x09\x80\xFB\x07" \
+        "\x49\x89\xD7\x49\x89\xF6\x49\x89\xFD\x66\x41\x8B\x5D\x08\xB3\x01\x74\x09\x80\xFB\x07"
+}
+
+function Pl()
+{
+    run \
+        'Patch Pl ...' \
+        '/Applications/Adobe Prelude CC 2019/Adobe Prelude CC 2019.app/Contents/Frameworks/Registration.framework/Versions/A/Registration' \
+        "\x49\x89\xD7\x49\x89\xF6\x49\x89\xFD\x66\x41\x8B\x5D\x08\x84\xDB\x74\x09\x80\xFB\x07" \
+        "\x49\x89\xD7\x49\x89\xF6\x49\x89\xFD\x66\x41\x8B\x5D\x08\xB3\x01\x74\x09\x80\xFB\x07"
+}
+
+function Ch()
+{
+    run \
+        'Patch Ch ...' \
+        '/Applications/Adobe Character Animator CC 2019/Adobe Character Animator CC 2019.app/Contents/MacOS/Character Animator' \
+        "\x49\x89\xD7\x49\x89\xF6\x49\x89\xFD\x66\x41\x8B\x5D\x08\x84\xDB\x74\x09\x80\xFB\x07" \
+        "\x49\x89\xD7\x49\x89\xF6\x49\x89\xFD\x66\x41\x8B\x5D\x08\xB3\x01\x74\x09\x80\xFB\x07"
+}
+
+function Ae()
+{
+    run \
+        'Patch Ae ...' \
+        '/Applications/Adobe After Effects CC 2019/Adobe After Effects CC 2019.app/Contents/Frameworks/AfterFXLib.framework/Versions/A/AfterFXLib' \
+        "\x66\x41\x8B\x5D\x08\x84\xDB\x74\x09\x80\xFB\x07" \
+        "\x66\x41\x8B\x5D\x08\xB3\x01\x74\x09\x80\xFB\x07"
+}
+
+function Me()
+{
+    run \
+        'Patch Ch ...' \
+        '/Applications/Adobe Media Encoder CC 2019/Adobe Media Encoder CC 2019.app/Contents/MacOS/Adobe Media Encoder CC 2019' \
+        "\x66\x41\x8B\x5D\x08\x84\xDB\x74\x09\x80\xFB\x07" \
+        "\x66\x41\x8B\x5D\x08\xB3\x01\x74\x09\x80\xFB\x07"
+}
+
+function Br()
+{
+    run \
+        'Patch Br ...' \
+        '/Applications/Adobe Bridge CC 2019/Adobe Bridge CC 2019.app/Contents/MacOS/Adobe Bridge CC 2019' \
+        "\x66\x41\x8B\x5E\x08\x84\xDB\x0F\x84\x0F\x01\x00\x00\x80\xFB\x07" \
+        "\x66\x41\x8B\x5E\x08\xB3\x01\x0F\x84\x0F\x01\x00\x00\x80\xFB\x07"
+}
+
+function An()
+{
+    run \
+        'Patch An ...' \
+        '/Applications/Adobe Animate CC 2019/Adobe Animate CC 2019.app/Contents/MacOS/Adobe Animate CC 2019' \
+        "\x66\x89\xD8\x66\x25\xFF\x00\x0F\x84\x71\x01\x00\x00\x0F\xB7\xC0\x83\xF8\x07" \
+        "\x66\x89\xD8\x90\x90\xB0\x01\x0F\x84\x71\x01\x00\x00\x0F\xB7\xC0\x83\xF8\x07"
+}
+
+function Dw()
+{
+    run \
+        'Patch Dw ...' \
+        '/Applications/Adobe Dreamweaver CC 2019/Adobe Dreamweaver CC 2019.app/Contents/MacOS/Dreamweaver' \
+        "\x66\x41\x8B\x5D\x08\x84\xDB\x74\x09\x80\xFB\x07" \
+        "\x66\x41\x8B\x5D\x08\xB3\x01\x74\x09\x80\xFB\x07"
+}
+
+# function Dn()
+# {
+#     echo 'Patch DN CC 19'
+#     DNCC19='/Applications/Adobe Dimension CC/Adobe Dimension CC.app/Contents/Frameworks/amtlib.framework/Versions/A/amtlib'
+#     if [ ! -f "$DNCC19.bak" ]; then
+#         sudo mv "$DNCC19" "$DNCC19.bak"
+#         sudo cp "$DNCC19.bak" "$DNCC19"
+#         echo "Backup file: $DNCC19.bak"
+#     fi
+#     sudo perl -pi -e 's|\x0F\xB6\x16\x48\x83\xFA\x20|\x90\x90\x90\x48\x83\xFA\x20|g' "$DNCC19"
+#     sudo perl -pi -e 's|\x48\x39\xF0\x75\xE2|\x48\x39\xF0\x90\x90|g' "$DNCC19"
+#     echo 'Patch complete'
+# }
+
+if [ $UID -ne 0 ]; then
+    sudo $0 $@
+    exit
 fi
-sudo perl -pi -e 's|\x0F\xB6\x16\x48\x83\xFA\x20|\x90\x90\x90\x48\x83\xFA\x20|g' "$DNCC19"
-sudo perl -pi -e 's|\x48\x39\xF0\x75\xE2|\x48\x39\xF0\x90\x90|g' "$DNCC19"
-echo 'Patch complete'
+
+if [ "$1" == "" ]; then
+    echo "All."
+    Ps
+    Lr
+    Ai
+    Id
+    Ic
+    Au
+    Pr
+    Pl
+    Ch
+    Ae
+    Me
+    Br
+    An
+    Dw
+    # Dn
+elif [ $(echo "$1" | tr [a-z] [A-Z]) == "PS" ]; then
+    Ps
+elif [ $(echo "$1" | tr [a-z] [A-Z]) == "LR" ]; then
+    Lr
+elif [ $(echo "$1" | tr [a-z] [A-Z]) == "AI" ]; then
+    Ai
+elif [ $(echo "$1" | tr [a-z] [A-Z]) == "ID" ]; then
+    Id
+elif [ $(echo "$1" | tr [a-z] [A-Z]) == "IC" ]; then
+    Ic
+elif [ $(echo "$1" | tr [a-z] [A-Z]) == "AU" ]; then
+    Au
+elif [ $(echo "$1" | tr [a-z] [A-Z]) == "PR" ]; then
+    Pr
+elif [ $(echo "$1" | tr [a-z] [A-Z]) == "PL" ]; then
+    Pl
+elif [ $(echo "$1" | tr [a-z] [A-Z]) == "CH" ]; then
+    Ch
+elif [ $(echo "$1" | tr [a-z] [A-Z]) == "AE" ]; then
+    Ae
+elif [ $(echo "$1" | tr [a-z] [A-Z]) == "ME" ]; then
+    Me
+elif [ $(echo "$1" | tr [a-z] [A-Z]) == "BR" ]; then
+    Br
+elif [ $(echo "$1" | tr [a-z] [A-Z]) == "AN" ]; then
+    An
+elif [ $(echo "$1" | tr [a-z] [A-Z]) == "DW" ]; then
+    Dw
+# elif [ $(echo "$1" | tr [a-z] [A-Z]) == "DN" ]; then
+#     Dn
+fi
