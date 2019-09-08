cb4d092054744191eaf236d4384e9c6c7a13b020
YJBeetle <yjbeetle@gmail.com>
2019-09-08 20:42:56 +0800

    tab to 4space

--- cc19patch.ps1
+++ cc19patch.ps1
@@ -1,81 +1,81 @@
 
 function patch($__file, $__find, $__to)
 {
-	# $find = ($__find -split ' ' | ForEach-Object {[char][byte]"0x$_"}) -join ''
-	# $to = ($__to -split ' ' | ForEach-Object {[char][byte]"0x$_"}) -join ''
-	[Byte[]] $find = $__find -split ' ' | ForEach-Object {[byte]"0x$_"}
-	[Byte[]] $to = $__to -split ' ' | ForEach-Object {[byte]"0x$_"}
-	# $to = [Byte[]](0x0F,0xB6,0x41,0x08,0xb0,0x01,0x74,0x0A,0x3C,0x07)
+    # $find = ($__find -split ' ' | ForEach-Object {[char][byte]"0x$_"}) -join ''
+    # $to = ($__to -split ' ' | ForEach-Object {[char][byte]"0x$_"}) -join ''
+    [Byte[]] $find = $__find -split ' ' | ForEach-Object {[byte]"0x$_"}
+    [Byte[]] $to = $__to -split ' ' | ForEach-Object {[byte]"0x$_"}
+    # $to = [Byte[]](0x0F,0xB6,0x41,0x08,0xb0,0x01,0x74,0x0A,0x3C,0x07)
 
-	[Byte[]] $file = [System.Io.File]::ReadAllBytes( $__file )
+    [Byte[]] $file = [System.Io.File]::ReadAllBytes( $__file )
 
-	$position = 0
-	for ($i = 0; $i -lt $file.length; $i++)
-	{
-		for ($ii = 0; $ii -lt $find.length; $ii++)
-		{
-			if($file[$i+$ii] -ne $find[$ii])
-			{
-				break
-			}
-		}
-		if ($ii -eq $find.length)
-		{
-			$position = $i
-			break
-		}
-	}
+    $position = 0
+    for ($i = 0; $i -lt $file.length; $i++)
+    {
+        for ($ii = 0; $ii -lt $find.length; $ii++)
+        {
+            if($file[$i+$ii] -ne $find[$ii])
+            {
+                break
+            }
+        }
+        if ($ii -eq $find.length)
+        {
+            $position = $i
+            break
+        }
+    }
 
-	if ( $position -ne 0 )
-	{
-		$fileStream = [System.IO.File]::Open($__file, [System.IO.FileMode]::Open, [System.IO.FileAccess]::Write, [System.IO.FileShare]::ReadWrite)
-		$binaryWriter = New-Object System.IO.BinaryWriter($fileStream)
-		$binaryWriter.BaseStream.Position = $position;
-		$binaryWriter.Write($to)
-		$fileStream.Close()
-    	( Get-FileHash "$__file" -Algorithm MD5 ).Hash > "$__file.patched.md5"
-	    echo "Patch succeeded."
-	}
-	else
-	{
-	    echo "Patch faild."
-	}
+    if ( $position -ne 0 )
+    {
+        $fileStream = [System.IO.File]::Open($__file, [System.IO.FileMode]::Open, [System.IO.FileAccess]::Write, [System.IO.FileShare]::ReadWrite)
+        $binaryWriter = New-Object System.IO.BinaryWriter($fileStream)
+        $binaryWriter.BaseStream.Position = $position;
+        $binaryWriter.Write($to)
+        $fileStream.Close()
+        ( Get-FileHash "$__file" -Algorithm MD5 ).Hash > "$__file.patched.md5"
+        echo "Patch succeeded."
+    }
+    else
+    {
+        echo "Patch faild."
+    }
 }
 
 function run($__tab, $__file, $__find, $__to)
 {
-	if ( Test-Path "$__file" -PathType Leaf )
-	{
+    if ( Test-Path "$__file" -PathType Leaf )
+    {
         echo "Found and patching $__tab ..."
-		if ( ( ! ( Test-Path "$__file.bak" -PathType Leaf ) ) -or ( ! ( Test-Path "$__file.patched.md5" -PathType Leaf ) ) -or ( ( cat "$__file.patched.md5" ) -ne ( Get-FileHash "$__file" -Algorithm MD5 ).Hash ) )
-		{
-			cp "$__file" "$__file.bak"
+        if ( ( ! ( Test-Path "$__file.bak" -PathType Leaf ) ) -or ( ! ( Test-Path "$__file.patched.md5" -PathType Leaf ) ) -or ( ( cat "$__file.patched.md5" ) -ne ( Get-FileHash "$__file" -Algorithm MD5 ).Hash ) )
+        {
+            cp "$__file" "$__file.bak"
             echo "Backup succeeded."
-			patch "$__file" "$__find" "$__to"
-		}
-		else
-		{
+            patch "$__file" "$__find" "$__to"
+        }
+        else
+        {
             echo "Already patched, skipped."
-		}
-	}
+        }
+    }
 }
 
 function _Ps()
 {
     run `
         'Ps' `
-		"C:\Program Files\Adobe\Adobe Photoshop CC 2019\Photoshop.exe" `
-		"0F B6 41 08 84 C0 74 0A 3C 07" `
-		"0F B6 41 08 B0 01 74 0A 3C 07"
+        "C:\Program Files\Adobe\Adobe Photoshop CC 2019\Photoshop.exe" `
+        "0F B6 41 08 84 C0 74 0A 3C 07" `
+        "0F B6 41 08 B0 01 74 0A 3C 07"
 }
 
 function _Lr()
 {
-	run `
-		'Lr' `
-		"C:\Program Files\Adobe\Adobe Lightroom Classic\Lightroom.exe" `
-		"0F B6 41 08 84 C0 74 0A 3C 07" `
-		"0F B6 41 08 B0 01 74 0A 3C 07"
+    run `
+        'Lr' `
+        "C:\Program Files\Adobe\Adobe Lightroom Classic\Lightroom.exe" `
+        "0F B6 41 08 84 C0 74 0A 3C 07" `
+        "0F B6 41 08 B0 01 74 0A 3C 07"
 }
 
 function _Ai()
@@ -83,8 +83,8 @@ function _Ai()
     run `
         'Ai' `
         'C:\Program Files\Adobe\Adobe Illustrator CC 2019\Support Files\Contents\Windows\Illustrator.exe' `
-		"0F B6 41 08 84 C0 74 0A 3C 07" `
-		"0F B6 41 08 B0 01 74 0A 3C 07"
+        "0F B6 41 08 84 C0 74 0A 3C 07" `
+        "0F B6 41 08 B0 01 74 0A 3C 07"
 }
 
 function _Id()
@@ -92,8 +92,8 @@ function _Id()
     run `
         'Id' `
         'C:\Program Files\Adobe\Adobe InDesign CC 2019\Public.dll' `
-		"0F B6 41 08 84 C0 74 0A 3C 07" `
-		"0F B6 41 08 B0 01 74 0A 3C 07"
+        "0F B6 41 08 84 C0 74 0A 3C 07" `
+        "0F B6 41 08 B0 01 74 0A 3C 07"
 }
 
 function _Ic()
@@ -101,8 +101,8 @@ function _Ic()
     run `
         'Ic' `
         'C:\Program Files\Adobe\Adobe InCopy CC 2019\Public.dll' `
-		"0F B6 41 08 84 C0 74 0A 3C 07" `
-		"0F B6 41 08 B0 01 74 0A 3C 07"
+        "0F B6 41 08 84 C0 74 0A 3C 07" `
+        "0F B6 41 08 B0 01 74 0A 3C 07"
 }
 
 function _Au()
